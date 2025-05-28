7da864fc8aaabdeb711edc59201b4c9830ed1cbf
Shararamosh <www.kuka@list.ru>
2025-05-28 23:28:03 +0300

    Removed NUITKA_ONEFILE_PARENT. Perhaps now Nuitka action will work fine with most recent version.

--- src/BDSP_ColorVariation_JSONParser/general.py
+++ src/BDSP_ColorVariation_JSONParser/general.py
@@ -14,13 +14,11 @@ RICH_CONSOLE = Console()
 
 def get_resource_path(file_path: str) -> str:
     """
-    Returns path to file or directory inside project, when used PyInstaller or Nuitka are used.
+    Returns path to file or directory inside project.
     :param file_path: Original path to file or directory.
     :return: Modified path to file or directory.
     """
-    if "NUITKA_ONEFILE_PARENT" in os.environ:
-        base_path = os.path.dirname(sys.executable)
-    elif hasattr(sys, "_MEIPASS"):
+    if hasattr(sys, "_MEIPASS"):  # PyInstaller.
         base_path = getattr(sys, "_MEIPASS")
     else:
         base_path = os.path.dirname(__file__)
